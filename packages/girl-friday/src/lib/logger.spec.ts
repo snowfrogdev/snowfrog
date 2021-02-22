aa522b1bdb968cfa4b8e3e8498338c15d26ce31c
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-02-21 21:58:20 -0500

    Don't send messages to transporter if loglevel is greater thant the one configure

--- packages/girl-friday/src/lib/logger.spec.ts
+++ packages/girl-friday/src/lib/logger.spec.ts
@@ -5,18 +5,39 @@ import { Transporter } from './transporter';
 
 
 describe('Logger', () => {
-  test('#info message is passed to registered transporters when logging levels set to info', () => {
-    let param1: string;
-    let param2: string;
-    const transporter1: Transporter = { write: (message) => param1 = message };
-    const transporter2: Transporter = { write: (message) => param2 = message };
+  test('#info message is passed to registered transporter when logging level set to info', () => {
+    let param: string;
+    const transporter: Transporter = { write: (message) => param = message };
 
-    const logger = new Logger([{ level: Level.Info, transporter: transporter1 }, { level: Level.Info, transporter: transporter2 }]);
+    const logger = new Logger([{ level: Level.Info, transporter: transporter }]);
     const message = 'This is a test';
 
     logger.info(message);
 
-    expect(param1).toBe(message);
-    expect(param2).toBe(message);
+    expect(param).toBe(message);
+  });
+
+  test('#info message is passed to registered transporter when logging level set to debug', () => {
+    let param: string;
+    const transporter: Transporter = { write: (message) => param = message };
+
+    const logger = new Logger([{ level: Level.Debug, transporter: transporter }]);
+    const message = 'This is a test';
+
+    logger.info(message);
+
+    expect(param).toBe(message);
+  });
+
+  test('#info message is not passed to registered transporter when logging level set to error', () => {
+    let hasBeenCalled = false;
+    const transporter: Transporter = { write: () => hasBeenCalled = true };
+
+    const logger = new Logger([{ level: Level.Error, transporter: transporter }]);
+    const message = 'This is a test';
+
+    logger.info(message);
+
+    expect(hasBeenCalled).toBeFalsy();
   });
 });
--- packages/girl-friday/src/lib/logger.ts
+++ packages/girl-friday/src/lib/logger.ts
@@ -1,8 +1,17 @@
+import { Level } from "./level";
 import { TransporterConfig } from "./transporter-config";
 
 export class Logger {
   constructor(private _configs: TransporterConfig[]) {}
   info(message: string): void {
-    this._configs.forEach(config => config.transporter.write(message));
+    this._logWith(Level.Info, message);
+  }
+
+  private _logWith(level: Level, message: string): void {
+    for (const config of this._configs) {
+      if (level <= config.level) {
+        config.transporter.write(message);
+      }
+    }
   }
 }
