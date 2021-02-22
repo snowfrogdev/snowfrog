b7fb602035d4cb0cc8b2b715e3aaa7b5a54b24a0
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-02-22 14:07:31 -0500

    remove example app

--- packages/girl-friday/src/index.ts
+++ packages/girl-friday/src/index.ts
@@ -1,1 +1,9 @@
+export * from './lib/level';
 export * from './lib/logger';
+export * from './lib/transporter';
+export * from './lib/transporter-config';
+export { Transporters };
+
+import { ConsoleTransporter } from './lib/transporters/console.transporter';
+
+const Transporters = { ConsoleTransporter: ConsoleTransporter };
